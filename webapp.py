1ad61f1ee93f85834593a42f77cf805a9dd4af8a
VinayMavi <vinaymavi@gmail.com>
2017-04-28 15:34:54 +0530

    First commit.

---
+++ webapp.py
@@ -0,0 +1,12 @@
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('My-Stats now under development.')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
