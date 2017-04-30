a70ea6c637d465c2ea7e9579ab4acceddef52d13
VinayMavi <vinaymavi@gmail.com>
2017-04-30 13:37:06 +0530

    dJango tempate included.

--- webapp.py
+++ webapp.py
@@ -1,10 +1,16 @@
 import webapp2
+import os
+from google.appengine.ext.webapp import template
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('My-Stats now under development.')
+        template_values = {
+            'msg': "Hello world from template :)",
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
 
 
 app = webapp2.WSGIApplication([
