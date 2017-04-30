dda987adedc31478e89cd086d709cb7089602c38
VinayMavi <vinaymavi@gmail.com>
2017-05-01 00:09:21 +0530

    Dashboard ready with rough graph.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,85 @@
 /**
  * main script to make ajax requests, handle responses and create graphs.
  */
+
+/**
+ * Draw chart.
+ * @param data
+ */
+function drawChart(data) {
+    var _data = [];
+    var keysArr = Object.keys(data);
+    for (var i = 0; i < keysArr.length; i++) {
+        var obj = data[keysArr[i]];
+        var name = obj.name;
+        var x = [];
+        var y = [];
+        obj.data.forEach(function (val, index) {
+            y.push(val.duration);
+            x.push(dateFormat(val.startTime));
+        });
+        _data.push({
+            x: x,
+            y: y,
+            mode: 'lines',
+            type: 'scatter',
+            name: name
+        });
+    }
+
+    function dateFormat(ts) {
+        var dateStr = "";
+        var d = new Date(ts);
+        dateStr = d.getFullYear() + "-";
+        dateStr = dateStr + (d.getMonth() + 1) + "-";
+        dateStr = dateStr + (d.getDate()) + " ";
+        dateStr = dateStr + (d.getHours()) + ":";
+        dateStr = dateStr + (d.getMinutes()) + ":";
+        dateStr = dateStr + (d.getSeconds()) + "";
+        return dateStr;
+    }
+
+    var layout = {
+        title: 'My Stats',
+        showlegend: true,
+        xaxis: {
+            type: 'date'
+        },
+        yaxis: {
+            title: 'Time',
+            type: 'linear'
+        }
+    };
+    Plotly.newPlot('my-chart', _data, layout, {showLink: false});
+}
+/**
+ * Process array of items and group single website data.
+ * @example
+ * {"<website_name>"{
+ * data:[]
+ * name:<website name>
+ * }}
+ * @param items {Array} of items
+ */
+function processItems(items) {
+    var data = {};
+    items.forEach(function (val, index) {
+        if (typeof data[val.domain] === "undefined") {
+            data[val.domain] = {
+                data: [],
+                name: val.domain
+            }
+        }
+        data[val.domain].data.push(val);
+    });
+    return data;
+}
+
+function init() {
+    gapi.client.load('greeting', 'v1', function () {
+        gapi.client.greeting.website_data.get().execute(function (resp) {
+            console.log(resp.items);
+            drawChart(processItems(resp.items));
+        });
+    }, "https://my-stats-ext.appspot.com/_ah/api");
+}
--- webapp.py
+++ webapp.py
@@ -9,10 +9,24 @@ class MainPage(webapp2.RequestHandler):
             'msg': "Hello world from template :)",
         }
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
         self.response.out.write(template.render(path, template_values))
+
+
+class LoginPage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/login.html')
+        self.response.out.write(template.render(path, {}));
+
+
+class DashBoard(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/dashboard.html')
+        self.response.out.write(template.render(path, {}));
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/login', LoginPage),
+    ('/dashboard', DashBoard)
 ], debug=True)
