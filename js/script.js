338f155463514403b4c82a2a70452717b6c1bcfd
VinayMavi <vinaymavi@gmail.com>
2017-05-01 15:45:18 +0530

    Display only top 10 sites in sorted order.

--- js/script.js
+++ js/script.js
@@ -6,19 +6,18 @@
  * Draw chart.
  * @param data
  */
-function drawChart(data) {
+function drawChart(websiteData) {
+    var data = websiteData[0];
+    var sortObj = websiteData[1];
     var _data = [];
-    var keysArr = Object.keys(data);
+    var keysArr = Object.keys(sortObj);
     for (var i = 0; i < keysArr.length; i++) {
-        if (i > 10) {
-            continue;
-        }
         var obj = data[keysArr[i]];
         var name = obj.name;
         var x = [];
         var y = [];
         obj.data.forEach(function (val, index) {
-            y.push(val.duration);
+            y.push(secToHours(val.duration));
             x.push(dateFormat(val.startTime));
         });
         _data.push({
@@ -42,6 +41,10 @@ function drawChart(data) {
         return dateStr;
     }
 
+    function secToHours(sec) {
+        return (sec / (60 * 60));
+    }
+
     var layout = {
         title: 'My Stats',
         showlegend: true,
@@ -49,7 +52,7 @@ function drawChart(data) {
             type: 'date'
         },
         yaxis: {
-            title: 'Time',
+            title: 'Time(Hours)',
             type: 'linear'
         }
     };
@@ -66,18 +69,44 @@ function drawChart(data) {
  */
 function processItems(items) {
     var data = {};
+    var durObj = {};
+    var sortArr;
     items.forEach(function (val, index) {
         if (typeof data[val.domain] === "undefined") {
             data[val.domain] = {
                 data: [],
                 name: val.domain
             }
+            durObj[val.domain] = {
+                duration: 0
+            }
         }
+        durObj[val.domain]['duration'] += val.duration;
         data[val.domain].data.push(val);
     });
-    return data;
+    console.log(durObj);
+    sortArr = sortByDur(durObj);
+    return [data, sortArr];
 }
 
+function sortByDur(obj) {
+    var keyArr = Object.keys(obj);
+    var sortableArr = [];
+    var sortObj = {};
+    keyArr.forEach(function (val) {
+        sortableArr.push([val, obj[val]['duration']]);
+    });
+    var sortArr = sortableArr.sort(function (a, b) {
+        return a[1] - b[1];
+    });
+    /* Convert to object first 10 sites.*/
+    console.log(sortArr);
+    for (var i = sortArr.length - 1; i > sortArr.length - 11; i--) {
+        sortObj[sortArr[i][0]] = sortArr[i][1];
+    }
+    console.log(sortObj);
+    return sortObj;
+}
 function init() {
     gapi.client.load('greeting', 'v1', function () {
         gapi.client.greeting.website_data.get().execute(function (resp) {
